5d126d03a70ff34e42556ca49c9be7028dcff0b8
sasank1-54 <114579796+sasank1-54@users.noreply.github.com>
2022-10-30 08:06:29 +0530

    Update ClassImpl.java

--- Java-Task-main/ClassImpl.java
+++ Java-Task-main/ClassImpl.java
@@ -6,7 +6,7 @@ public class ClassImpl {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Teacher tr = new Teacher("Siva",22,50000);
+		Teacher tr = new Teacher("Sasank",22,5000);
 		tr.getDetails();
 		
 	}
